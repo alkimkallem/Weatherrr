5ff9c4a79cfeeb73eb6be7126e034339d7fdf14b
alkimkallem <1335013@dogus.edu.tr>
2017-11-01 21:39:03 +0300

    dgfhzvhbj

---
+++ app/src/main/java/edu/dogus/weatherrr/WeatherActivity.java
@@ -0,0 +1,8 @@
+package edu.dogus.weatherrr;
+
+/**
+ * Created by alkimkallem on 1.11.2017.
+ */
+
+public class WeatherActivity {
+}
---
+++ app/src/main/java/edu/dogus/weatherrr/json/WeatherResults.java
@@ -0,0 +1,8 @@
+package edu.dogus.weatherrr.json;
+
+/**
+ * Created by alkimkallem on 1.11.2017.
+ */
+
+public class WeatherResults {
+}
